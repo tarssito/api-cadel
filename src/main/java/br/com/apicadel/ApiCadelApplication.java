5bcec1c351a3949db2623461b7377386f2ca3442
Italo <italobrazil7@gmail.com>
2018-06-02 18:04:09 -0300

    Ajuste no dump

--- src/main/java/br/com/apicadel/ApiCadelApplication.java
+++ src/main/java/br/com/apicadel/ApiCadelApplication.java
@@ -80,10 +80,14 @@ public class ApiCadelApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Curso si = new Curso(null, "Sistema de Informação");
+		//Cursos
+		Curso si = new Curso(null, "Sistemas de Informação");
 		Curso cc = new Curso(null, "Ciência da Computação");
+		Curso ec = new Curso(null, "Engenharia da Computação");
+		Curso ea = new Curso(null, "Engenharia Ambiental");
 
-		Disciplina laboratorioDeSoftware = new Disciplina(null, "Laboratorio de Software", 60);
+		//Disciplinas
+		Disciplina laboratorioDeSoftware = new Disciplina(null, "Laboratório de Software", 60);
 		Disciplina gestaoProjeto = new Disciplina(null, "Gestão de Projetos", 50);
 		Disciplina redes = new Disciplina(null, "Redes", 80);
 		Disciplina sd = new Disciplina(null, "Sistemas Distribuídos", 80);
@@ -91,69 +95,50 @@ public class ApiCadelApplication implements CommandLineRunner {
 		Disciplina so = new Disciplina(null, "Sistemas Operacionais", 80);
 		Disciplina ihc = new Disciplina(null, "IHC", 80);
 
-		Turma turma1 = new Turma(null, "SI011", "1", "2018", si, TurnoLetivo.MATUTINO.getTurno(),
-				laboratorioDeSoftware);
-		Turma turma2 = new Turma(null, "CC011", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), laboratorioDeSoftware);
-
-		Turma turma3 = new Turma(null, "SI012", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), gestaoProjeto);
-		Turma turma4 = new Turma(null, "CC012", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), gestaoProjeto);
-
-		Turma turma5 = new Turma(null, "SI013", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), redes);
-		Turma turma6 = new Turma(null, "CC013", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), redes);
-
-		Turma turma7 = new Turma(null, "SI014", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), sd);
-		Turma turma8 = new Turma(null, "CC014", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), sd);
-
-		Turma turma9 = new Turma(null, "SI015", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), bdII);
-		Turma turma10 = new Turma(null, "CC015", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), bdII);
-
-		Turma turma11 = new Turma(null, "SI016", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), so);
-		Turma turma12 = new Turma(null, "CC016", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), so);
-
-		Turma turma13 = new Turma(null, "SI017", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), ihc);
-		Turma turma14 = new Turma(null, "CC017", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), ihc);
-
-		disciplinaRepository.saveAll(Arrays.asList(laboratorioDeSoftware, gestaoProjeto, redes, sd, bdII, so, ihc));
-		cursoRepository.saveAll(Arrays.asList(si, cc));
-
+		//Relação Curso-Disciplina
 		CursoDisciplina cd1 = new CursoDisciplina(null, si, laboratorioDeSoftware);
 		CursoDisciplina cd2 = new CursoDisciplina(null, si, gestaoProjeto);
 		CursoDisciplina cd3 = new CursoDisciplina(null, si, redes);
 		CursoDisciplina cd4 = new CursoDisciplina(null, si, sd);
-
+		CursoDisciplina cd9 = new CursoDisciplina(null, si, bdII);
+		CursoDisciplina cd11 = new CursoDisciplina(null, si, so);
+		
 		CursoDisciplina cd5 = new CursoDisciplina(null, cc, laboratorioDeSoftware);
+		CursoDisciplina cd8 = new CursoDisciplina(null, cc, ihc);
 		CursoDisciplina cd6 = new CursoDisciplina(null, cc, redes);
 		CursoDisciplina cd7 = new CursoDisciplina(null, cc, sd);
-		CursoDisciplina cd8 = new CursoDisciplina(null, cc, ihc);
-
-		CursoDisciplina cd9 = new CursoDisciplina(null, si, bdII);
 		CursoDisciplina cd10 = new CursoDisciplina(null, cc, bdII);
-
-		CursoDisciplina cd11 = new CursoDisciplina(null, si, so);
 		CursoDisciplina cd12 = new CursoDisciplina(null, cc, so);
+		
+		//Alunos
+		Aluno aluno1 = new Aluno(null, "Davi Erick Filipe Ferreira", "13521250997", "042151000", "ddavierickfilipeferreira@comunicacao.com.br", "M");
+		Aluno aluno2 = new Aluno(null, "Sebastião Leonardo Márcio Oliveira", "53460054980", "042151001", "sebastiaoleonador@hotmail.com", "M");
+		Aluno aluno3 = new Aluno(null, "Louise Antônia Rita Ribeiro", "93724007698", "042151002", "louise.arr@hotmail.com", "F");
+		Aluno aluno4 = new Aluno(null, "Renato Benício Galvão", "48173711372", "042151003", "renatobenicio@hotmail.com", "M");
+		Aluno aluno5 = new Aluno(null, "Calebe José Ryan da Conceição", "42183029951", "042151004", "caleberyan@hotmail.com", "M");
+		Aluno aluno6 = new Aluno(null, "Henry Marcos Ramos", "31139200941", "042151005", "henryramos@hotmail.com", "M");
+		Aluno aluno7 = new Aluno(null, "Noah Daniel Luan Brito", "61589610601", "042151006", "noahbrito@hotmail.com", "M");
+		Aluno aluno8 = new Aluno(null, "Fernando Anderson Enrico Monteiro", "14710985405", "042151007", "enricoanders@hotmail.com", "M");
+		Aluno aluno9 = new Aluno(null, "Mariah Luna Raimunda Moreira", "40236200925", "042151008", "mariah@hotmail.com", "F");
+		Aluno aluno10 = new Aluno(null, "Márcia Rosa Catarina Rodrigues", "47271096892", "042151009", "marciarosa@hotmail.com", "F");
+		
+		//Professor
+		Professor prof1 = new Professor(null, "Admin", "12345678982", "admin", "admin@gmail.com", "M", true, 5,
+				Perfil.ADMIN, BCrypt.hashpw("admin123", BCrypt.gensalt()), si);
 
-		cursoDisciplinaRepository.saveAll(Arrays.asList(cd1, cd2, cd3, cd4, cd5, cd6, cd7, cd8, cd9, cd10, cd11, cd12));
+		Professor prof2 = new Professor(null, "Victor Thomas Moura", "82026562490", "22145000", "victor@gmail.com", "M", true,
+				5, Perfil.PROFESSOR, BCrypt.hashpw("82026562", BCrypt.gensalt()), si);
 
-		turmaRepository.saveAll(Arrays.asList(turma1, turma2, turma3, turma4, turma5, turma6, turma7, turma8, turma9,
-				turma10, turma11, turma12, turma13, turma14));
+		Professor prof3 = new Professor(null, "Juan Ryan Cavalcanti", "33618093802", "22145001", "juanryan@gmail.com", "M",
+				true, 5, Perfil.PROFESSOR, BCrypt.hashpw("33618093", BCrypt.gensalt()), si);
 
-		Professor prof1 = new Professor(null, "Admin", "00000000000", "admin", "admin@gmail.com", "M", true, 5,
-				Perfil.ADMIN, BCrypt.hashpw("admin", BCrypt.gensalt()), si);
+		Professor prof4 = new Professor(null, "Benício Jorge Pinto", "45382932450", "22145002", "benicio@gmail.com", "M", true, 5,
+				Perfil.PROFESSOR, BCrypt.hashpw("45382932", BCrypt.gensalt()), si);
 
-		Professor prof2 = new Professor(null, "Sandro Alex", "05049493000", "22145000", "sandro@gmail.com", "M", true,
-				5, Perfil.PROFESSOR, BCrypt.hashpw("admin", BCrypt.gensalt()), si);
-
-		Professor prof3 = new Professor(null, "Eduardo Xavier", "05049493900", "22145001", "xavier@gmail.com", "M",
-				true, 5, Perfil.PROFESSOR, BCrypt.hashpw("admin", BCrypt.gensalt()), si);
-
-		Professor prof4 = new Professor(null, "Alex Coelho", "05049493922", "22145002", "alex@gmail.com", "M", true, 5,
+		Professor prof5 = new Professor(null, "Débora Stefany de Paula", "96875568624", "22145003", "deborasp@gmail.com", "F", true, 5,
 				Perfil.PROFESSOR, BCrypt.hashpw("admin", BCrypt.gensalt()), si);
-
-		Professor prof5 = new Professor(null, "Arthur", "05049493888", "22145003", "arthur@gmail.com", "M", true, 5,
-				Perfil.PROFESSOR, BCrypt.hashpw("admin", BCrypt.gensalt()), si);
-
-		professorRepository.saveAll(Arrays.asList(prof1, prof2, prof3, prof4, prof5));
-
+		
+		//Relação Professor Disciplina
 		ProfessorDisciplina pd1 = new ProfessorDisciplina(null, prof1, laboratorioDeSoftware);
 		ProfessorDisciplina pd2 = new ProfessorDisciplina(null, prof2, redes);
 		ProfessorDisciplina pd3 = new ProfessorDisciplina(null, prof3, sd);
@@ -161,52 +146,47 @@ public class ApiCadelApplication implements CommandLineRunner {
 		ProfessorDisciplina pd5 = new ProfessorDisciplina(null, prof3, bdII);
 		ProfessorDisciplina pd6 = new ProfessorDisciplina(null, prof4, gestaoProjeto);
 		ProfessorDisciplina pd7 = new ProfessorDisciplina(null, prof5, ihc);
+		
+		//Turmas
+		Turma si001LS = new Turma(null, "SI001", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), laboratorioDeSoftware);
+		Turma si001GP = new Turma(null, "SI001", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), gestaoProjeto);
+		Turma si001LR = new Turma(null, "SI001", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), redes);
+		Turma si001IHC = new Turma(null, "SI001", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), ihc);
+		Turma si001SD = new Turma(null, "SI001", "1", "2018", si, TurnoLetivo.NOTURNO.getTurno(), sd);
 
-		professorDisciplinaRepository.saveAll(Arrays.asList(pd1, pd2, pd3, pd4, pd5, pd6, pd7));
+		Turma cc001LS = new Turma(null, "CC001", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), laboratorioDeSoftware);
+		Turma cc001GP = new Turma(null, "CC001", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), gestaoProjeto);
+		Turma cc001LR = new Turma(null, "CC001", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), redes);
+		Turma cc001SD = new Turma(null, "CC001", "1", "2018", cc, TurnoLetivo.NOTURNO.getTurno(), sd);
 
-		Aluno aluno1 = new Aluno(null, "Tarssito Pereira", "03035268584", "042151013", "tarssito@hotmail.com", "M");
-		Aluno aluno2 = new Aluno(null, "Ítalo Brazil", "03035268583", "042151012", "italo@hotmail.com", "M");
-		Aluno aluno3 = new Aluno(null, "Ian Ítalo", "03035268582", "042151011", "ian@hotmail.com", "M");
-		Aluno aluno4 = new Aluno(null, "Marcos Reis", "03035268581", "042151015", "marcos@hotmail.com", "M");
-		Aluno aluno5 = new Aluno(null, "Gabriel Rebouças", "03035268587", "042151016", "gabriel@hotmail.com", "M");
-		Aluno aluno6 = new Aluno(null, "Thiago Vinhas", "03035268589", "042151017", "vinhas@hotmail.com", "M");
-
-		Aluno aluno7 = new Aluno(null, "Jeferson Souza", "02235268584", "042151023", "jeferson@hotmail.com", "M");
-		Aluno aluno8 = new Aluno(null, "José Veríssimo", "12035268583", "042151022", "ze@hotmail.com", "M");
-		Aluno aluno9 = new Aluno(null, "Danilo Reis", "15035268582", "042151021", "danilo@hotmail.com", "M");
-		Aluno aluno10 = new Aluno(null, "Igor Carmo", "12032338581", "042151025", "rael@hotmail.com", "M");
-		Aluno aluno11 = new Aluno(null, "Wasgton Silva", "10035268587", "042151026", "wasgton@hotmail.com", "M");
-		Aluno aluno12 = new Aluno(null, "Ricardo Oliveira", "03035268900", "042151027", "ricardo@hotmail.com", "M");
-
-		alunoRepository.saveAll(Arrays.asList(aluno1, aluno2, aluno3, aluno4, aluno5, aluno6, aluno7, aluno8, aluno9,
-				aluno10, aluno11, aluno12));
-
-		AlunoTurma alunoTurma1 = new AlunoTurma(null, aluno1, turma1);
-		AlunoTurma alunoTurma2 = new AlunoTurma(null, aluno2, turma1);
-		AlunoTurma alunoTurma3 = new AlunoTurma(null, aluno3, turma1);
-		AlunoTurma alunoTurma4 = new AlunoTurma(null, aluno4, turma1);
-		AlunoTurma alunoTurma5 = new AlunoTurma(null, aluno5, turma1);
-		AlunoTurma alunoTurma6 = new AlunoTurma(null, aluno6, turma1);
-
-		alunoTurmaRepository
-				.saveAll(Arrays.asList(alunoTurma1, alunoTurma2, alunoTurma3, alunoTurma4, alunoTurma5, alunoTurma6));
-
-		/**
-		 * Dump de classe
-		 */
-		Classe a1 = new Classe(null, DiaSemana.SEGUNDA, TurnoLetivo.NOTURNO, "18:45", "20:15", "1", "2018", si, prof1,
+		//ALUNO 1 EM TODAS AS DISCIPLINAS DA TURMA SI001
+		AlunoTurma aluno1Turma1 = new AlunoTurma(null, aluno1, si001LS);
+		AlunoTurma aluno1Turma2 = new AlunoTurma(null, aluno1, si001GP);
+		AlunoTurma aluno1Turma3 = new AlunoTurma(null, aluno1, si001LR);
+		AlunoTurma aluno1Turma4 = new AlunoTurma(null, aluno1, si001IHC);
+		AlunoTurma aluno1Turma5 = new AlunoTurma(null, aluno1, si001SD);
+		
+		//ALUNO 2 EM 4 DISCIPLINAS DA TURMA SI001 E EM UMA DISCIPLINA DA TURMA CC001
+		AlunoTurma aluno2Turma1 = new AlunoTurma(null, aluno2, si001LS);
+		AlunoTurma aluno2Turma2 = new AlunoTurma(null, aluno2, si001GP);
+		AlunoTurma aluno2Turma3 = new AlunoTurma(null, aluno2, si001LR);
+		AlunoTurma aluno2Turma4 = new AlunoTurma(null, aluno2, si001IHC);
+		AlunoTurma aluno2Turma5 = new AlunoTurma(null, aluno2, cc001SD);
+		
+		//Classes
+		Classe a1 = new Classe(null, DiaSemana.SEGUNDA, TurnoLetivo.NOTURNO, "18:45", "20:15", "1", "2018", si, prof2,
 				laboratorioDeSoftware);
-		Classe a2 = new Classe(null, DiaSemana.SEGUNDA, TurnoLetivo.NOTURNO, "20:15", "21:50", "1", "2018", si, prof1,
+		Classe a2 = new Classe(null, DiaSemana.SEGUNDA, TurnoLetivo.NOTURNO, "20:15", "21:50", "1", "2018", si, prof2,
 				laboratorioDeSoftware);
 
-		Classe a3 = new Classe(null, DiaSemana.TERCA, TurnoLetivo.NOTURNO, "18:45", "20:15", "1", "2018", si, prof4,
+		Classe a3 = new Classe(null, DiaSemana.TERCA, TurnoLetivo.NOTURNO, "18:45", "20:15", "1", "2018", si, prof3,
 				gestaoProjeto);
-		Classe a4 = new Classe(null, DiaSemana.TERCA, TurnoLetivo.NOTURNO, "20:15", "21:50", "1", "2018", si, prof4,
+		Classe a4 = new Classe(null, DiaSemana.TERCA, TurnoLetivo.NOTURNO, "20:15", "21:50", "1", "2018", si, prof3,
 				gestaoProjeto);
 
-		Classe a5 = new Classe(null, DiaSemana.QUARTA, TurnoLetivo.NOTURNO, "18:45", "20:15", "1", "2018", si, prof2,
+		Classe a5 = new Classe(null, DiaSemana.QUARTA, TurnoLetivo.NOTURNO, "18:45", "20:15", "1", "2018", si, prof4,
 				redes);
-		Classe a6 = new Classe(null, DiaSemana.QUARTA, TurnoLetivo.NOTURNO, "20:15", "21:50", "1", "2018", si, prof2,
+		Classe a6 = new Classe(null, DiaSemana.QUARTA, TurnoLetivo.NOTURNO, "20:15", "21:50", "1", "2018", si, prof4,
 				redes);
 
 		Classe a7 = new Classe(null, DiaSemana.QUINTA, TurnoLetivo.NOTURNO, "18:45", "20:15", "1", "2018", si, prof5,
@@ -214,41 +194,53 @@ public class ApiCadelApplication implements CommandLineRunner {
 		Classe a8 = new Classe(null, DiaSemana.QUINTA, TurnoLetivo.NOTURNO, "20:15", "21:50", "1", "2018", si, prof5,
 				ihc);
 
-		Classe a9 = new Classe(null, DiaSemana.SEXTA, TurnoLetivo.NOTURNO, "18:45", "20:15", "1", "2018", si, prof3,
+		Classe a9 = new Classe(null, DiaSemana.SABADO, TurnoLetivo.NOTURNO, "18:45", "20:15", "1", "2018", si, prof2,
 				sd);
-		Classe a10 = new Classe(null, DiaSemana.SEXTA, TurnoLetivo.NOTURNO, "20:15", "21:50", "1", "2018", si, prof3,
+		Classe a10 = new Classe(null, DiaSemana.SABADO, TurnoLetivo.NOTURNO, "20:15", "21:50", "1", "2018", si, prof2,
 				sd);
+		
+		//Relação Classe-Turma
+		ClasseTurma at1 = new ClasseTurma(null, a1, si001LS);
+		ClasseTurma at2 = new ClasseTurma(null, a2, si001LS);
+		
+		ClasseTurma at3 = new ClasseTurma(null, a3, si001GP);
+		ClasseTurma at4 = new ClasseTurma(null, a4, si001GP);
 
+		ClasseTurma at5 = new ClasseTurma(null, a5, si001LR);
+		ClasseTurma at6 = new ClasseTurma(null, a6, si001LR);
+
+		ClasseTurma at7 = new ClasseTurma(null, a7, si001IHC);
+		ClasseTurma at8 = new ClasseTurma(null, a8, si001IHC);
+		
+		ClasseTurma at9 = new ClasseTurma(null, a8, si001SD);
+		ClasseTurma at10 = new ClasseTurma(null, a9, si001SD);
+		ClasseTurma at11 = new ClasseTurma(null, a8, cc001SD);
+		ClasseTurma at12 = new ClasseTurma(null, a9, cc001SD);
+		
+		cursoRepository.saveAll(Arrays.asList(si, cc, ec, ea));
+		
+		disciplinaRepository.saveAll(Arrays.asList(laboratorioDeSoftware, gestaoProjeto, redes, sd, bdII, so, ihc));
+		
+		cursoDisciplinaRepository.saveAll(Arrays.asList(cd1, cd2, cd3, cd4, cd5, cd6, cd7, cd8, cd9, cd10, cd11, cd12));
+		
+		alunoRepository.saveAll(Arrays.asList(aluno1, aluno2, aluno3, aluno4, aluno5, aluno6, aluno7, aluno8, aluno9, aluno10));
+		
+		professorRepository.saveAll(Arrays.asList(prof1, prof2, prof3, prof4, prof5));
+		
+		professorDisciplinaRepository.saveAll(Arrays.asList(pd1, pd2, pd3, pd4, pd5, pd6, pd7));
+		
+		turmaRepository.saveAll(Arrays.asList(si001LS, si001GP, si001LR, si001IHC, si001SD, cc001LS, cc001GP, cc001LR, cc001SD));
+		
+		alunoTurmaRepository.saveAll(Arrays.asList(aluno1Turma1, aluno1Turma2, aluno1Turma3, aluno1Turma4, aluno1Turma5,
+				aluno2Turma1, aluno2Turma2, aluno2Turma3, aluno2Turma4, aluno2Turma5));
+		
 		classeRepository.saveAll(Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10));
-
-		ClasseTurma at1 = new ClasseTurma(null, a1, turma1);
-		ClasseTurma at2 = new ClasseTurma(null, a1, turma2);
-		ClasseTurma at3 = new ClasseTurma(null, a2, turma1);
-		ClasseTurma at4 = new ClasseTurma(null, a2, turma2);
-
-		ClasseTurma at5 = new ClasseTurma(null, a3, turma3);
-		ClasseTurma at7 = new ClasseTurma(null, a4, turma3);
-
-		ClasseTurma at9 = new ClasseTurma(null, a5, turma5);
-		ClasseTurma at10 = new ClasseTurma(null, a6, turma6);
-		ClasseTurma at11 = new ClasseTurma(null, a5, turma5);
-		ClasseTurma at12 = new ClasseTurma(null, a6, turma6);
-
-		ClasseTurma at13 = new ClasseTurma(null, a7, turma13);
-		ClasseTurma at14 = new ClasseTurma(null, a8, turma13);
-
-		ClasseTurma at15 = new ClasseTurma(null, a5, turma7);
-		ClasseTurma at16 = new ClasseTurma(null, a6, turma8);
-		ClasseTurma at17 = new ClasseTurma(null, a5, turma7);
-		ClasseTurma at18 = new ClasseTurma(null, a6, turma8);
-
-		classeTurmaRepository.saveAll(
-				Arrays.asList(at1, at2, at3, at4, at5, at7, at9, at10, at11, at12, at13, at14, at15, at16, at17, at18));
+		
+		classeTurmaRepository.saveAll(Arrays.asList(at1, at2, at3, at4, at5, at7, at9, at10, at11, at12));
 		
 		LocalDate dataDeInscricao = LocalDate.now();
 		Date data = Date.from(dataDeInscricao.atStartOfDay(ZoneId.systemDefault()).toInstant());
 		
-		Aula aula1 = new Aula(null, data, a1, StatusAula.ABERTA);
 		//emailService.sendOrderConfirmationEmail(aula1);
 	}
 
